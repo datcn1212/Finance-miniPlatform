c602a89b7c2f42613600224d701f2c38bef35738
datcn1212 <datcn.12.2001@gmail.com>
2023-01-04 08:49:34 +0700

    update main, requirement

--- src/main/main.py
+++ src/main/main.py
@@ -1,7 +1,9 @@
 #-------------------------MAIN PROGRAM------------------------------------
-
 import logging.config
 import configparser
+
+from vnstock import * 
+import pandas
 
 #-------------------Set logging config------------------------------------
 logging.basicConfig(level = logging.DEBUG, filename="./logs/log.txt", format='---%(asctime)s: %(message)s', filemode='a')
@@ -10,3 +12,29 @@ logger = logging.getLogger()
 #-------------------Read config file--------------------------------------
 config = configparser.ConfigParser()
 config.read('./src/config/config.ini')
+
+
+#-------------------Get data from API-------------------------------------
+
+listing_companies()
+
+ticker_overview('TCB')
+
+df =  stock_historical_data(symbol='GMD', start_date="2021-01-01", end_date='2022-09-22')
+
+price_board('TCB,SSI,VND') # so sanh cac ticker
+
+stock_intraday_data(symbol='GMD', page_num=0, page_size=5000) # giao dich trong ngay
+
+#------------------Push got data to Redis---------------------------------- 
+
+
+#------------------Get data from Redis and insert to Spark/Hadoop----------
+
+
+#------------------Insert to Elasticsearch---------------------------------
+
+
+#------------------Apache Airflow------------------------------------------
+
+
